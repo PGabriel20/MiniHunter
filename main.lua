9a1b20138f52409f530caaca2ab3549fccb9c012
PGabriel20 <bielporreca@gmail.com>
2022-06-12 19:00:55 -0300

    Added option to leave game

--- main.lua
+++ main.lua
@@ -48,6 +48,12 @@ function love.load()
   
 end
 
+function love.keypressed(key, unicode)
+  if key == "escape" then
+    love.event.quit()
+  end
+end
+
 function love.update(dt)
   local isPlayerMoving = false
 
